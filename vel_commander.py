4c1b78d878e23b0477416445e72f4df94cc3090f
HasanFarag <53587583+HasanFarag@users.noreply.github.com>
2020-01-19 17:43:14 +0100

    Update vel_commander.py

--- vel_commander.py
+++ vel_commander.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import Twist
 import math
 import tf
 from tf.transformations import euler_from_quaternion
+import sys
 class robot(object):
     def __init__(self,robot_no):
         self.pub = rospy.Publisher('robot' + str(robot_no) + '/cmd_vel', Twist, queue_size=10)
@@ -23,37 +24,47 @@ class robot(object):
         
 
 if __name__ == '__main__':
-    rospy.init_node('cat_mouse_game', anonymous=True)
+    rospy.init_node('cat_mouse_game', anonymous=True,disable_signals=False)
     cat = robot(1)
     mouse = robot(2)
     tf_listener = tf.TransformListener()
     rate = rospy.Rate(10) # 10hz
     first_time = True
-    while not rospy.is_shutdown():
-        start = rospy.get_time()
-        mouse.move(0.1,0.0)
+    while not rospy.is_shutdown():    
         if first_time:
             tf_listener.waitForTransform('/robot1/odom','/robot2/odom',rospy.Time(0), rospy.Duration(5.0))
+            start_time = rospy.get_time()
             first_time = False
+        # cat_ang_vel <= 0.2 
+        cat.move(0.7,cat_ang_vel)
+        mouse.move(0.2,mouse_ang_vel)
+        # mouse_ang_vel <= 0.9
         tf_listener.waitForTransform('/robot1/robot_footprint','/robot2/robot_footprint',rospy.Time(0), rospy.Duration(1.0))
         (trans,rot) = tf_listener.lookupTransform('/robot1/robot_footprint', '/robot2/robot_footprint', rospy.Time(0))
         frame_shift = 1
-        print('x',trans[0]-frame_shift)
-        print('y',trans[1]-frame_shift)
+        #print('x',trans[0]-frame_shift)
+        #print('y',trans[1]-frame_shift)
         inter_distance = math.sqrt((trans[0]-frame_shift) ** 2 + (trans[1]-frame_shift) ** 2)
+        #print('inter-distance is ' + str(inter_distance))
         inter_angle_rpy =  euler_from_quaternion(rot)
         inter_angle = inter_angle_rpy[2]
-        time_elapsed = rospy.get_time()-start
-        print('inter-distance is ' + str(inter_distance))
-        print('inter-angle is ' + str(inter_angle))
-        print('elapsed time is ' + str(time_elapsed))
-            #
-            #mouse.move(5.0,4.0)
-            #cat.stop()
-            #mouse.stop()
+        #print('inter-angle is ' + str(inter_angle))
+        elapsed_time = rospy.get_time() - start_time
+        print('elapsed time is ' + str(elapsed_time))
+        if inter_distance < 0.5 and elapsed_time < 60.0:
+            print('The cat won the game')
+            cat.stop()
+            mouse.stop()
+            rospy.signal_shutdown('Shutdown')
+            sys.exit(1)
+        elif elapsed_time > 60.0:  
+            print('The mouse won the game')
+            cat.stop()
+            mouse.stop()  
+            rospy.signal_shutdown('Shutdown')
+            sys.exit(1)                                                                                                                                                                                                                                                                                                                                                                                                                              
         rate.sleep()
-    try:
-        rospy.spin() 
-    except rospy.ROSInterruptException:
-        pass
+    
+    rospy.spin() 
+    
    
